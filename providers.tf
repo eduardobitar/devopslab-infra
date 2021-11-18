0b9b7161db519b6118faa0a605a571ae42c2e027
eduardobitar <85189390+eduardobitar@users.noreply.github.com>
2021-11-18 15:20:40 -0300

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.89.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+
+  project = "labdevopscloudeduardobitar"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
